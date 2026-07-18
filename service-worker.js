851dbd6dab319d2e271551860a3a1984b3904ba1
silver9204 <silver9204@users.noreply.github.com>
2026-07-18 19:39:03 +0900

    Align home section headings and cards

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "la-trip-v6";
+const CACHE_NAME = "la-trip-v7";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
