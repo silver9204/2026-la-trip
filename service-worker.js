42e28bf39b7ba6624ba6fc74e2ac131ddccbf9a9
silver9204 <silver9204@users.noreply.github.com>
2026-07-19 14:08:08 +0900

    출국일 환전 수령 일정 추가

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "la-trip-v32";
+const CACHE_NAME = "la-trip-v33";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => {
